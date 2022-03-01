89f914251b9ccd5fdced0f3d9c1245004874fe23
Anna Kummer <annamkummer@gmail.com>
2022-03-01 15:01:38 -0700

    Install cypress and add initial tests

---
+++ cypress/integration/article_spec.js
@@ -0,0 +1,19 @@
+describe('Article View', () => {
+    beforeEach(() => {
+
+    })
+  
+    it('As a user, when I visit the article page, I should see the title, byline, and abstract for the article I selected.', () => {
+
+    })
+  
+    it('As a user, when I visit the article page, I should see a link to the full article on the nyt website', () => {
+
+    })
+  
+    it('As a user, when I visit the article page, I should see a home button that takes me back to the landing page when clicked.', () => {
+        // URL change
+    })
+
+  
+})
---
+++ cypress/integration/landing_spec.js
@@ -0,0 +1,24 @@
+describe('Landing View', () => {
+    beforeEach(() => {
+
+    })
+  
+    it('As a user, when I visit the landing page, I should see a page title, a dropdown menu, and a list of articles.', () => {
+
+    })
+  
+    it('As a user, when I visit the landing page, the dropdown should be set to all, and I should be able to select another category.', () => {
+
+    })
+  
+    it('As a user, when I select a category from the dropdown, I should see the list of articles change to only include articles with that category.', () => {
+        // URL change
+    })
+  
+    it('As a user, when I visit the landing page, I should be able to click on an article title and be taken to the detailed view for that article.', () => {
+        // URL change
+    })
+
+  
+})
+  
