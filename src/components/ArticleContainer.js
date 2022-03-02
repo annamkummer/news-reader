157f4d890a08b35af99f1b6f3e3d82094a625834
Anna Kummer <annamkummer@gmail.com>
2022-03-02 09:31:36 -0700

    Handle fetch errors

--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -3,22 +3,28 @@ import { Link } from 'react-router-dom'
 
 function ArticleContainer({ articles }) {
 
-  const allArticles = articles.map((article, i) => {
-    let id;
+  let articleList;
 
-    if (!article.short_url.length) {
-      const url = article.url.split('/')
-      id = url[url.length - 1].split('.')[0]
-    } else {
-      const shortURL = article.short_url.split('/')
-      id = shortURL[shortURL.length - 1]
-    }
-    return <Link key={i} className="article-title" to={`/article/${id}`}>{article.title}</Link>
-  })
+  if (articles instanceof Error) {
+    articleList = articles.message
+  } else {
+    articleList = articles.map((article, i) => {
+      let id;
+  
+      if (!article.short_url.length) {
+        const url = article.url.split('/')
+        id = url[url.length - 1].split('.')[0]
+      } else {
+        const shortURL = article.short_url.split('/')
+        id = shortURL[shortURL.length - 1]
+      }
+      return <Link key={i} className="article-title" to={`/article/${id}`}>{article.title}</Link>
+    })
+  }
 
   return (
     <div className="article-view">
-      {allArticles}
+      {articleList}
     </div>
   );
 }
--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,17 +1,25 @@
 export const getStories = (category) => {
     return fetch(`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=JjtHnKuJ2fsaSKglmDtq3XsdKTUxRqe2`)
         .then(response => response.json())
-        .then(data => data.results)
-        .catch(err => console.log(err))
+        .then(data => {
+            if (data.status === "OK") {
+                return data.results
+            }
+            throw new Error("Hmmm, something went wrong. Please check your internet connection and try again.")
+        })
+        .catch(err => err)
 }
 
 export const getStory = (id) => {
     return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=JjtHnKuJ2fsaSKglmDtq3XsdKTUxRqe2`)
         .then(response => response.json())
         .then(data => {
-            return data.results.find(article => {
-                return article.short_url.includes(id) || article.url.includes(id)
-            })
+            if (data.status === "OK") {
+                return data.results.find(article => {
+                    return article.short_url.includes(id) || article.url.includes(id)
+                })
+            }
+            throw new Error("Hmmm, something went wrong. Please check your internet connection and try again.")
         })
-        .catch(err => console.log(err))
+        .catch(err => err)
 }
